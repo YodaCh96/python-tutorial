d6e302a045896357e50264ba5694e6a7c76bd20f
Ioannis Christodoulakis <ioannis.ch@outlook.com>
2023-07-14 21:31:44 +0200

    update the python tutorial

---
+++ output.py
@@ -0,0 +1,30 @@
+# Output Variables
+
+x = "Ioannis"
+
+print(x)
+
+x = "Ioannis"
+y = "Christodoulakis"
+
+print(x, y)
+
+x = "Ioannis"
+y = "Christodoulakis"
+
+print(x + y)
+
+x = 15
+y = 10
+
+print(x + y)
+
+x = 28
+y = "Ioannis"
+
+print(x, y)
+
+x = 28
+y = "Ioannis"
+
+# print(x + y) ERROR
